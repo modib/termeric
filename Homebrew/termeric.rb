fc04297334245ffb12e656a3ab9b076cf99b1044
B. Modi <bhavin@mbp-m2>
2026-05-31 02:21:38 -0700

    fix: update Homebrew formula SHA256 after bug fix release

--- Homebrew/termeric.rb
+++ Homebrew/termeric.rb
@@ -2,7 +2,7 @@ class Termeric < Formula
   desc "Golden prompts for your terminal"
   homepage "https://modib.github.io/termeric/"
   url "https://github.com/modib/termeric/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "PLACEHOLDER_SHA256"
+  sha256 "3f2f89eb32a6cca24e767124516bbdcbc7e1ff5888cb7d4b6fbc2ab53fdbed46"
   license "MIT"
   head "https://github.com/modib/termeric.git", branch: "main"
 
